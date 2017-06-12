6eb2a396e26ebbdd566e59f54ecb15c415a62f4b
CJ R <cj@null.computer>
2017-06-12 08:08:31 -0600

    preload images

--- avocado.js
+++ avocado.js
@@ -1,12 +1,15 @@
 const button = document.querySelector('button');
-const avocado = document.querySelector('img');
+const avocadoSFW = document.querySelector('#sfw');
+const avocadoNSFW = document.querySelector('#nsfw');
 
 button.addEventListener('click', () => {
   if(button.textContent == 'NSFW MODE') {
-    avocado.src = 'avocado-nsfw.png';
     button.textContent = 'SFW MODE';
+    avocadoSFW.style.display = 'none';
+    avocadoNSFW.style.display = '';
   } else {
-    avocado.src = 'avocado-sfw.png';
     button.textContent = 'NSFW MODE';
+    avocadoSFW.style.display = '';
+    avocadoNSFW.style.display = 'none';
   }
 });
